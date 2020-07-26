3ceb9ea1481c577e132cd518851a853c4efad3cf
Kaliary Cisne de Sousa da Silva <kaliarycisne@gmail.com>
2020-07-26 15:24:06 -0300

    render

--- src/main.js
+++ src/main.js
@@ -2,13 +2,14 @@ class App {
     constructor() {
         this.repositories = [];
 
-        this.formEll = document.getElementById('repo-form');
+        this.formEl = document.getElementById('repo-form');
+        this.listEl = document.getElementById('repo-list');
 
         this.registerHandlers();
     }
 
     registerHandlers() {
-        this.formEll.onsubmit = event => this.addRepository(event);
+        this.formEl.onsubmit = event => this.addRepository(event);
     }
 
     addRepository(event) {
@@ -21,7 +22,34 @@ class App {
             html_url: "https://github.com/Rocketseat/nlw-01-booster"
         });
 
-        console.log(this.repositories);
+        this.render();
+    }
+
+    render() {
+        this.listEl.innerHTML = '';
+
+        this.repositories.forEach(repo  => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            descriptionEl.appendChild(document.createTextNode('Go'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+        });
     }
 }
 
