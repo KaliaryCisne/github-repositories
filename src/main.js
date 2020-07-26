791f70473872ac9de96ddf86e76d101cfc079438
Kaliary Cisne de Sousa da Silva <kaliarycisne@gmail.com>
2020-07-26 17:02:34 -0300

    search api

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
+import api from './api';
+
 class App {
     constructor() {
         this.repositories = [];
 
         this.formEl = document.getElementById('repo-form');
+        this.inputEl = document.querySelector('input[name=repository]');
         this.listEl = document.getElementById('repo-list');
 
         this.registerHandlers();
@@ -12,16 +15,28 @@ class App {
         this.formEl.onsubmit = event => this.addRepository(event);
     }
 
-    addRepository(event) {
+    async addRepository(event) {
         event.preventDefault();
+
+        const repoInput = this.inputEl.value;
+
+        if (repoInput.length === 0)
+            return;
         
+        const response = await api.get(`/repos/${repoInput}`);
+        console.log(response);
+
+        const { name, description, html_url, owner: { avatar_url } } = response.data;
+
         this.repositories.push({
-            name: "rocketseat.com.br",
-            description: "Here is a description",
-            avatar_url: "https://avatars0.githubusercontent.com/u/28929274?v=4",
-            html_url: "https://github.com/Rocketseat/nlw-01-booster"
+            name,
+            description,
+            avatar_url,
+            html_url,
         });
 
+        this.inputEl.value = '';
+        
         this.render();
     }
 
@@ -40,7 +55,8 @@ class App {
 
             let linkEl = document.createElement('a');
             linkEl.setAttribute('target', '_blank');
-            descriptionEl.appendChild(document.createTextNode('Go'));
+            linkEl.setAttribute('href', repo.html_url);
+            linkEl.appendChild(document.createTextNode('Go'));
 
             let listItemEl = document.createElement('li');
             listItemEl.appendChild(imgEl);
