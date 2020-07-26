7b2eb5795a73ea11b4e983b8033e9c6c5545cc77
Kaliary Cisne de Sousa da Silva <kaliarycisne@gmail.com>
2020-07-26 11:57:34 -0300

    add repositories

--- src/main.js
+++ src/main.js
@@ -1,1 +1,28 @@
-alert('Hello world!');
+class App {
+    constructor() {
+        this.repositories = [];
+
+        this.formEll = document.getElementById('repo-form');
+
+        this.registerHandlers();
+    }
+
+    registerHandlers() {
+        this.formEll.onsubmit = event => this.addRepository(event);
+    }
+
+    addRepository(event) {
+        event.preventDefault();
+        
+        this.repositories.push({
+            name: "rocketseat.com.br",
+            description: "Here is a description",
+            avatar_url: "https://avatars0.githubusercontent.com/u/28929274?v=4",
+            html_url: "https://github.com/Rocketseat/nlw-01-booster"
+        });
+
+        console.log(this.repositories);
+    }
+}
+
+new App();
